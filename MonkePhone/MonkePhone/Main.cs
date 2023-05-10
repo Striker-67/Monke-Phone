7484a4e207acbc79eb8b5a000d1c2e48067974ff
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-05-10 13:20:23 -0400

    BaseUnityPlugin

---
+++ MonkePhone/MonkePhone/Main.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using MonkePhone.Util;
+
+namespace MonkePhone
+{
+    [BepInPlugin(GUID, NAME, VERSION), BepInDependency("org.legoandmars.gorillatag.utilla")]
+    internal class Main : BaseUnityPlugin
+    {
+        internal const string
+            GUID = "striker.monkephone",
+            NAME = "Monke Phone",
+            VERSION = "1.0.0";
+        internal static ManualLogSource manualLogSource; // This allows the logger object to be accessed outside of this type - crafterbot
+        private void Awake()
+        {
+            manualLogSource = Logger; // to log messages in bold use ``loglevel message`` - crafterbot
+            $"Init : {NAME}".Log();
+        }
+    }
+}
---
+++ MonkePhone/MonkePhone/Util/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx.Logging;
+
+namespace MonkePhone.Util
+{
+    internal static class Extensions
+    {
+        internal static void Log(this object obj, LogLevel logLevel = LogLevel.Info)
+        {
+#if DEBUG
+            Main.manualLogSource.Log(logLevel, obj);
+#endif
+        }
+    }
+}
